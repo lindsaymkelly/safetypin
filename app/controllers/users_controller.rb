defa93dd4463d412f9a8d4cc0a5a4a15dbe02db6
lindsaymkelly <lindzlady@gmail.com>
2016-12-09 09:02:23 -0500

    Add initial user/sessions controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user 
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :city)
+    end
+
+
+end
+
